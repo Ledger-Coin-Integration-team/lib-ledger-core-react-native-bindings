ce61a041ba957609b0c1f54e93e88558f1a2d923
Henri Ly <henri@softlydev.com>
2020-07-31 11:36:23 +0200

    [Algorand] update to last algo libcore

--- preinstall.sh
+++ preinstall.sh
@@ -6,7 +6,7 @@
 #                              Preinstall script
 #
 
-LIB_CORE_VERSION="3.4.1-rc-210a68"
+LIB_CORE_VERSION="3.4.1-rc-2d10a3"
 BASE_URL="https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core/int-algorand-libcorev1/release"
 
 function main() {
